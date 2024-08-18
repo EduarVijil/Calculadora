2f74a163d4780013d7177384c3dccd09db31ea68
EduarVijil <eduardo.vijil@unah.hn>
2024-08-18 12:59:55 -0600

    Add files via upload

---
+++ Calculadora_1/src/calculadora_1/Calculadora_1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora_1;
+
+/**
+ *
+ * @author Informatica
+ */
+public class Calculadora_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculadora mical = new Calculadora();
+        mical.setVisible(true);
+    }
+    
+}
